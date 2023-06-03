f4f9b594b9ba4952faab316d6fe086580664873d
ris269 <ristudio269@gmail.com>
2023-06-03 15:24:43 +0700

    Add public files / Init Navbar / Init Auth Page

---
+++ codilityclone/src/pages/auth/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type AuthPageProps = {
+    
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    
+    return <div>Authentication Page</div>
+}
+export default AuthPage;
