261b9b2469255b91c4e96f1898ddcd8e2802bcc0
ris269 <ristudio269@gmail.com>
2023-06-05 21:58:19 +0700

    Update Youtube video modal

--- codilityclone/src/components/ProblemsTable/ProblemsTable.tsx
+++ codilityclone/src/components/ProblemsTable/ProblemsTable.tsx
@@ -1,6 +1,6 @@
 import { problems } from '@/mockProblems/problems';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsCheckCircle } from 'react-icons/bs'
 import { AiFillYoutube } from 'react-icons/ai'
 import { IoClose } from 'react-icons/io5';
@@ -10,6 +10,23 @@ type ProblemsTableProps = {
 };
 
 const ProblemsTable: React.FC<ProblemsTableProps> = () => {
+    const [youtubePlayer, setYoutubePlayer] = useState({
+        isOpen: false,
+        videoId: ''
+    })
+
+    const closeYoutubeModal = () => {
+        setYoutubePlayer({ isOpen: false, videoId: '' })
+    }
+
+    useEffect(() => {
+        const handleEsc = (e: KeyboardEvent) => {
+            if (e.key === 'Escape') closeYoutubeModal()
+        }
+        window.addEventListener('keydown', handleEsc)
+
+        return () => { window.removeEventListener('keydown', handleEsc) }
+    })
 
     return (
         <>
@@ -34,7 +51,11 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
                             </td>
                             <td className={`px-6 py-4`}>
                                 {doc.videoId ? (
-                                    <AiFillYoutube fontSize={'28'} className='cursor-pointer hover:text-red-600' />
+                                    <AiFillYoutube
+                                        fontSize={'28'}
+                                        className='cursor-pointer hover:text-red-600'
+                                        onClick={() => setYoutubePlayer({ isOpen: true, videoId: doc.videoId as string })}
+                                    />
                                 ) : (
                                     <p className='text-gray-400'>Coming soon</p>
                                 )}
@@ -43,18 +64,24 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
                     )
                 })}
             </tbody>
-
-            <tfoot className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center ' >
-                <div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute'></div>
-                <div className='w-full z-50 h-full px-6 relative max-w-4xl'>
-                    <div className='w-full h-full flex items-center justify-center relative'>
-                        <div className='w-full relative'>
-                            <IoClose fontSize={"35"} className='cursor-pointer absolute -top-16 right-0' />
-                            <YouTube videoId={"xty7fr-k0TU"} loading='lazy' iframeClassName='w-full min-h-[500px]' />
+            {youtubePlayer.isOpen && (
+                <tfoot
+                    className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center'
+                    onClick={closeYoutubeModal}>
+                    <div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute'></div>
+                    <div className='w-full z-50 h-full px-6 relative max-w-4xl'>
+                        <div className='w-full h-full flex items-center justify-center relative'>
+                            <div className='w-full relative'>
+                                <IoClose
+                                    fontSize={"35"}
+                                    className='cursor-pointer absolute -top-16 right-0'
+                                    onClick={closeYoutubeModal} />
+                                <YouTube videoId={youtubePlayer.videoId} loading='lazy' iframeClassName='w-full min-h-[500px]' />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </tfoot>
+                </tfoot>
+            )}
         </>
 
     )
