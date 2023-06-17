ce3fc66d9d974c9f01420a8843b0f3c01501528f
ris269 <ristudio269@gmail.com>
2023-06-17 22:06:14 +0700

    Install React Code Mirror

--- codilityclone/src/components/Playground/Playground.tsx
+++ codilityclone/src/components/Playground/Playground.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
+import Split from 'react-split';
 
 type PlaygroundProps = {
 
@@ -8,9 +9,10 @@ type PlaygroundProps = {
 const Playground: React.FC<PlaygroundProps> = () => {
 
     return (
-        <>
+        <div className='flex flex-col bg-dark-layer-1 relative'>
             <PreferenceNav />
-        </>
+            <Split></Split>
+        </div>
     )
 }
 export default Playground;
