b5c4d4ff0b19d2e756cb0357b45c1b4553c995f0
ris269 <ristudio269@gmail.com>
2023-06-17 18:14:08 +0700

    init workspace component

---
+++ codilityclone/src/components/Workspace/Workspace.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type WorkspaceProps = {
+    
+};
+
+const Workspace:React.FC<WorkspaceProps> = () => {
+    
+    return <div>Workspace</div>
+}
+export default Workspace;
