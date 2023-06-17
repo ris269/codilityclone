859e0d34b786c38db606145b0410f67457d31a1e
ris269 <ristudio269@gmail.com>
2023-06-17 18:48:47 +0700

    adding Split css

---
+++ codilityclone/src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ProblemDescriptionProps = {
+    
+};
+
+const ProblemDescription:React.FC<ProblemDescriptionProps> = () => {
+    
+    return <div>Problem description</div>
+}
+export default ProblemDescription;
--- codilityclone/src/components/Workspace/Workspace.tsx
+++ codilityclone/src/components/Workspace/Workspace.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import Split from 'react-split';
+import ProblemDescription from './ProblemDescription/ProblemDescription';
 type WorkspaceProps = {
-    
+
 };
 
-const Workspace:React.FC<WorkspaceProps> = () => {
-    
-    return <div>Workspace</div>
+const Workspace: React.FC<WorkspaceProps> = () => {
+
+    return (
+        <Split className='split'>
+            <ProblemDescription />
+
+            <div>This is code editor area</div>
+        </Split>)
 }
 export default Workspace;
